f5bbb88a371c629c244cd3dfebff8d9c34d65e13
ahnhyosung <allsdfgh88@gmail.com>
2015-08-26 16:31:08 +0900

    test

---
+++ Telecommuting/src/admin/test.java
@@ -0,0 +1,56 @@
+package admin;
+
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class test extends JPanel {
+	private JTextField textField_name; // 이름 텍스트 필드
+	private JButton button_regist; // 얼굴 등록 버튼
+
+	/**
+	 * Create the panel.
+	 */
+	public test() {
+		setLayout(null);
+		setBackground(Color.WHITE);
+
+		JLabel lblNewLabel = new JLabel("이름");
+		lblNewLabel.setBounds(23, 107, 57, 15);
+		add(lblNewLabel);
+
+		textField_name = new JTextField();
+		textField_name.setBounds(59, 104, 116, 21);
+		add(textField_name);
+		textField_name.setColumns(10);
+
+		button_regist = new JButton("얼굴 등록");
+		button_regist.addActionListener(new AdminActionListener());
+		button_regist.setBounds(59, 155, 97, 23);
+		add(button_regist);
+		
+		JPanel panel = new FaceTrackingView();
+		panel.setBounds(187, 10, 780, 599);
+		add(panel);
+		
+		
+
+	}
+
+	class AdminActionListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			if (e.getSource() == button_regist) { // 파일 메뉴의 등록 아이템 선택 시
+				System.out.println("패턴 등록 완료!");
+
+			}
+		}
+
+	}
+}
