db7c4760a036fad45dc6c62746188b683ee344f1
ahnhyosung <allsdfgh88@gmail.com>
2015-08-28 01:26:36 +0900

    으아아아아아

--- Telecommuting/src/admin/TFaceRecord.java
+++ Telecommuting/src/admin/TFaceRecord.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 
+import javax.swing.JOptionPane;
+
 import Luxand.FSDK;
 import Luxand.FSDK.HImage;
 import Luxand.FSDK.TFacePosition;
@@ -17,12 +19,28 @@ public class TFaceRecord {
 	public String ImageFileName;
 	public FSDK.HImage image;
 	public FSDK.HImage faceImage;
-	
+
 	public final java.util.List<TFaceRecord> FaceList = new ArrayList<TFaceRecord>();
 	public int FaceDetectionThreshold = 3;
 	public int FARValue = 100;
 	public static final int width = 640;
 	public static final int height = 480;
+	
+	public TFaceRecord() {
+		try {
+            int r = FSDK.ActivateLibrary("bPOFbM5ekk9uCr9x75NEbpkV8UsG/NAoExxkGAk+POsNvUVdz0IdfXKog23Y89TnCneozsWdtKoaj2J2OdM9q9iNuUgTBU10gLNqcnUvxeuKZDcGzfaUmyUCIkqfnE/MbLpIuprI8Bvauj2B5NU6xu3trvDnhszFPA2/JyKdstk=");
+            if (r != FSDK.FSDKE_OK){
+               JOptionPane.showMessageDialog(main.MainFrame.frame, "Please run the License Key Wizard (Start - Luxand - FaceSDK - License Key Wizard)", "Error activating FaceSDK", JOptionPane.ERROR_MESSAGE); 
+               System.exit(r);
+            }
+        } 
+        catch(java.lang.UnsatisfiedLinkError e) {
+            JOptionPane.showMessageDialog(main.MainFrame.frame, e.toString(), "Link Error", JOptionPane.ERROR_MESSAGE);
+            System.exit(1);
+        } 
+		
+		FSDK.Initialize();
+	}
 
 	public void menuEnrollFace() {
 		// Assuming that faces are vertical (HandleArbitraryRotations = false)
@@ -31,58 +49,58 @@ public class TFaceRecord {
 		FSDK.SetFaceDetectionThreshold(FaceDetectionThreshold);
 
 		File f = new File("C:\\Temp\\facematch\\");
-	       
-        String fileName = null;
-        
-        System.out.println(f.length());
-        
-		for(String fileList:f.list()){
-            fileName = fileList;
-            
-            System.out.println(fileName);
-            TFaceRecord fr = new TFaceRecord();
-            fr.ImageFileName = fileName;
-            fr.image = new HImage();
-            
-            int res = FSDK.LoadImageFromFileW(fr.image, "C:\\Temp\\facematch\\"+fileName);
-            
-            fr.FacePosition = new TFacePosition.ByReference();
-            res = FSDK.DetectFace(fr.image, fr.FacePosition);
-            
-            fr.faceImage = new HImage(); // face이미지만 따로 설정
-            FSDK.CreateEmptyImage(fr.faceImage);
-            FSDK.CopyRect(fr.image,
-            		(int) (fr.FacePosition.xc - fr.FacePosition.w * 0.5),
-            		(int) (fr.FacePosition.yc - fr.FacePosition.w * 0.5),
-            		(int) (fr.FacePosition.xc + fr.FacePosition.w * 0.5),
-            		(int) (fr.FacePosition.yc + fr.FacePosition.w * 0.5),
-            		fr.faceImage);
-            
-            fr.FacialFeatures = new FSDK.FSDK_Features.ByReference();
-            res = FSDK.DetectEyesInRegion(fr.image, fr.FacePosition,
-            		fr.FacialFeatures);
-            
-            fr.FaceTemplate = new FSDK.FSDK_FaceTemplate.ByReference();
-            res = FSDK.GetFaceTemplateInRegion(fr.image, fr.FacePosition,
-            		fr.FaceTemplate);
-            
-            FaceList.add(fr);
-            if(FaceList != null)
-            	System.out.println("이미지가 저장됩니다!");
-            
-        }
+
+		String fileName = null;
+
+		System.out.println(f.length());
+
+		for (String fileList : f.list()) {
+			fileName = fileList;
+
+			System.out.println(fileName);
+			TFaceRecord fr = new TFaceRecord();
+			fr.ImageFileName = fileName;
+			fr.image = new HImage();
+
+			int res = FSDK.LoadImageFromFileW(fr.image, "C:\\Temp\\facematch\\"
+					+ fileName);
+
+			fr.FacePosition = new TFacePosition.ByReference();
+			res = FSDK.DetectFace(fr.image, fr.FacePosition);
+
+			fr.faceImage = new HImage(); // face이미지만 따로 설정
+			FSDK.CreateEmptyImage(fr.faceImage);
+			FSDK.CopyRect(fr.image,
+					(int) (fr.FacePosition.xc - fr.FacePosition.w * 0.5),
+					(int) (fr.FacePosition.yc - fr.FacePosition.w * 0.5),
+					(int) (fr.FacePosition.xc + fr.FacePosition.w * 0.5),
+					(int) (fr.FacePosition.yc + fr.FacePosition.w * 0.5),
+					fr.faceImage);
+
+			fr.FacialFeatures = new FSDK.FSDK_Features.ByReference();
+			res = FSDK.DetectEyesInRegion(fr.image, fr.FacePosition,
+					fr.FacialFeatures);
+
+			fr.FaceTemplate = new FSDK.FSDK_FaceTemplate.ByReference();
+			res = FSDK.GetFaceTemplateInRegion(fr.image, fr.FacePosition,
+					fr.FaceTemplate);
+
+			FaceList.add(fr);
+			if (FaceList != null)
+				System.out.println("이미지가 저장됩니다!");
+
+		}
 		System.out.println(FaceList.size());
 
-
 	}
-	
+
 	public class Sortable {
 		public float similarity;
 		public int index;
 	}
 
 	public void menuMatchFace() {
-
+		
 		String fileName = "C:\\temp\\in\\in.jpg";
 
 		TFaceRecord fr = new TFaceRecord();
@@ -116,18 +134,31 @@ public class TFaceRecord {
 
 		java.util.List<Sortable> sim_ind = new ArrayList<Sortable>();
 		float Similarity = 0;
+
+		// ///////////////////////////////
+		File f = new File("C:\\Temp\\facematch\\");
+
+		String fileName2 = null;
+
+		for (String fileList : f.list()) {
+			fileName2 = fileList;
+
+			System.out.println("test : " + fileName2);
+		}
+		// ///////////////////////////////
+
 		for (int i = 0; i < FaceList.size(); ++i) {
 			FSDK.MatchFaces(fr.FaceTemplate, FaceList.get(i).FaceTemplate,
 					SimilarityByReference);
 			Similarity = SimilarityByReference[0];
 			System.out.println(i + "번째 : " + Similarity);
-			if (Similarity >= 0.90) {
+			if (Similarity >= Threshold) {
 				Sortable s = new Sortable();
 				s.index = i;
 				s.similarity = Similarity;
 				sim_ind.add(s);
 				++MatchedCount;
-				System.out.println("입장 가능!!!! 얼굴이 확인 되었다."+Similarity);
+				System.out.println("입장 가능!!!! 얼굴이 확인 되었다." + Similarity);
 			}
 		}
 
